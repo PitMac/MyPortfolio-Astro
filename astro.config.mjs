b01b49ffeed2af274a3b3162a008630c1cbf274f
PitMac <jhondavidpitapita@hotmail.com>
2022-10-01 12:51:21 -0500

    change vercel conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), react()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
