0aed2bfc9d1fa905b38c1e1260a21eb87a06d401
PitMac <jhondavidpitapita@hotmail.com>
2022-10-01 13:00:43 -0500

    fixed paths & projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
